583200a65d1bfb0bd05274ccb12765933cce316b
Tania Omelinska <omelinska@gmail.com>
2018-10-21 18:09:03 +0300

    refactoring of unit test code in app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,11 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -14,20 +17,21 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  beforeEach(async(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   }));
   it(`should have as title 'sombra-angular-test'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('sombra-angular-test');
+    component = fixture.debugElement.componentInstance;
+    expect(component.title).toEqual('sombra-angular-test');
   }));
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to sombra-angular-test!');
+    component = fixture.debugElement.nativeElement;
+    expect(component.querySelector('h1').textContent).toContain('Welcome to sombra-angular-test!');
   }));
 });
